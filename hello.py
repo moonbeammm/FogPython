355539bb37a99e0848e5ec56665c351eda7b7814
sunguangxin <sunguangxin@bilibili.com>
2016-08-04 18:57:15 +0800

    1.python基础语法

---
+++ hello.py
@@ -0,0 +1,41 @@
+#!/Usr/bin/env python 
+
+# code 1
+# print 'hello world'
+# print '100 + 200 =', 100 + 200
+# print 100 + 200
+
+
+# code 2
+# name = raw_input('please enter your name:')
+# print 'hello' , name
+
+# if name == 'sunguangxin':
+# 	print 'i love you'
+# else :
+# 	print 'hehe'
+
+
+# code 3
+# print 'Hello, %s .you have : %d$', % ('world', 1000000)
+
+# code 4
+# clasmates = ['michael', 'bob', 'tracy']
+# print clasmates
+# print clasmates[0]
+
+# code 5
+# age = 3
+# if age > 18 :
+# 	print 'you are a adult'
+# else :
+# 	print 'you are a children'
+
+# code 6
+age = raw_input('your age is :')
+print age
+age = int(age)
+if age < 20:	
+	print 'children'
+else :
+	print 'adult'
