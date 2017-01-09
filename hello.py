2d101c2bd3c1b54086542ad49ee029d3e39838ae
sunguangxin <sunguangxin@bilibili.com>
2017-01-09 23:03:09 +0800

    sgx: 修改部分

--- hello.py
+++ hello.py
@@ -19,8 +19,12 @@
 # code 3
 # print 'Hello, %s .you have : %d$', % ('world', 1000000)
 
-# code 4
+# code 4 ------list
 # clasmates = ['michael', 'bob', 'tracy']
+# clasmates = [1,2,2,3]
+# clasmates = (1,2,3,3)
+# clasmates = {'ss' : 1, 'dd' : 2}
+# clasmates = set([1,2,2,3,3])
 # print clasmates
 # print clasmates[0]
 
@@ -35,22 +39,49 @@
 # age = raw_input('your age is :')
 # print age
 # age = int(age)
-# if age < 20:	
+# if age < 20:
 # 	print 'children'
 # else :
 # 	print 'adult'
 
-# code 7 dict 
-# dict = {'name' : 'sun', 'age' : 12}
-# dict['sun'] = 'guang'
-# print dict
+# code 7
+# print abs(-1)
 
-# dict.pop('name')
-# print dict
 
-# code 8 set
-# set = set([1,1,1,1,2])
-# print set
+#code 8
+# print cmp(1, 3)
+
+# code 9
+# print int(12.3)
+
+# code 10
+# a = abs
+# print a(-1)
+
+# code 11
+# def my_abs(x):
+# 	if not isinstance(x, (int, float)):
+# 		raise TypeError('input pram is error')
+# 	if x >= 0:
+# 		return x
+# 	else:
+# 		return -x
+# print my_abs('-22')
+
+# code 12
+# import math
+
+# print math.sin(math.pi / 6)
+
+# code 13
+# def multiReturn(x):
+# 	x += 2
+# 	y = x + 11
+# 	return x, y
+# x, y = multiReturn(10)
+# r = multiReturn(10)
+# print x, y
+# print r
 
 
 
