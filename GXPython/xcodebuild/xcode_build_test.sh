dd7dc32f94df501a2f632fd5ac519c0f15b5f69a
sunguangxin <sunguangxin@bilibili.com>
2017-09-19 16:43:18 +0800

    1.update

--- GXPython/xcodebuild/xcode_build_test.sh
+++ GXPython/xcodebuild/xcode_build_test.sh
@@ -2,12 +2,18 @@
 
 echo '请输入静态库工程目录!'
 # 例:/Users/bilibili/Desktop/GXPython/GXPython
-read lib_project_path
+read project_path
 
 echo '请输入静态库工程输出目录'
-# 例:/Users/bilibili/Desktop
-read lib_project_output_path
+# 例:/Users/bilibili/Desktop/GXLib
+read output_path
 
 echo '请输入静态库工程名称!'
 # GXPython
-read lib_project_name
+read project_name
+
+# 创建输出目录，并删除之前的文件
+#rm -rf "${output_path}"
+mkdir -p "${output_path}"
+
+xcodebuild -project ${project_name}".xcodeproj" -scheme ${project_name} -configuration Release -sdk iphoneos clean build
